39f5644863b0c2474a83bfac5c8c2baf76f278e3
presw <pmwhitehall@gmail.com>
2019-07-24 15:53:30 -0700

    Completed helper functions

--- src/Board.js
+++ src/Board.js
@@ -62,14 +62,14 @@
     },
 
 
-/*
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
     \__ \ || (_| | |  | |_  | | | |  __/ | |  __/_
     |___/\__\__,_|_|   \__| |_| |_|\___|_|  \___(_)
 
- */
+    */
     /*=========================================================================
     =                 TODO: fill in these Helper Functions                    =
     =========================================================================*/
@@ -130,27 +130,57 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var sum = 0;
+      var columnIndex = majorDiagonalColumnIndexAtFirstRow;
+      for (var i = 0; i < this.get('n'); i++) {
+        if (columnIndex >= 0) {
+          sum += this.get(i)[columnIndex];
+          if (sum > 1) {
+            return true;
+          }
+        }
+        columnIndex++;
+      }
+      return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      for (var i = (1-this.get('n')); i < this.get('n'); i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
-
-
 
     // Minor Diagonals - go from top-right to bottom-left
     // --------------------------------------------------------------
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var sum = 0;
+      var columnIndex = minorDiagonalColumnIndexAtFirstRow;
+      for (var i = 0; i < this.get('n'); i++) {
+        if (columnIndex < this.get('n')) {
+          sum += this.get(i)[columnIndex];
+          if (sum > 1) {
+            return true;
+          }
+        }
+        columnIndex--;
+      }
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      for (var i = 0; i < (this.get('n')*2-1); i++) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
