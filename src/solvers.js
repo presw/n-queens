030cb09dfe62c65816d46157ee72888c19aad8e5
presw <pmwhitehall@gmail.com>
2019-07-24 19:15:31 -0700

    Completed findNRooksSolution; added helper functions

--- src/solvers.js
+++ src/solvers.js
@@ -15,10 +15,74 @@
 
 
 
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+window.findNRooksSolution = function(n, piecesCount = 0, board, nextValidCoords = [0, 0]) {
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  var solution = undefined;
+
+  if (arguments.length === 1) {
+    board = new Board({n: n});
+  }
+
+  var rowIndex = nextValidCoords[0];
+  var columnIndex = nextValidCoords[1];
+
+  while (rowIndex < n) {
+    for (var i = columnIndex; i < n; i++) {
+
+      var newBoard = cloneBoard(n, board);
+      newBoard.togglePiece(rowIndex, i);
+      if (!newBoard.hasAnyRowConflicts() && !newBoard.hasAnyColConflicts()) {
+        piecesCount++;
+        var newValidCoords = [];
+        if (i + 1 >= n) {
+          newValidCoords[0] = rowIndex + 1;
+          newValidCoords[1] = 0;
+        } else {
+          newValidCoords[0] = rowIndex;
+          newValidCoords[1] = i + 1;
+        }
+
+        if (piecesCount < n) {
+          solution = findNRooksSolution(n, piecesCount, newBoard, newValidCoords);
+          if (Array.isArray(solution)) {
+            console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+            return solution;
+          }
+        } else {
+          return createSolution(n, newBoard);
+        }
+      }
+    }
+    rowIndex++;
+    columnIndex = 0;
+  }
+  // if arguments.length === 1 {
+  // board = new Board(n);
+  // }
+
+  // rowIndex = nextValidCoords[0];
+  // columnIndex = nextValidCoords[1];
+  /*
+  // while row (dependant on nextValidCoords) < n
+  // for loop to add pieces (use nextValidCoords for starting for loop)
+    // place a piece starting at nextValidCoords;
+    // check for validity - column and row
+      // increment count
+      // set nextValidCoords (current row and index + 1) // 0, 2 = 0, 3
+        // if index >= n
+          // nextRow = row + 1
+          // nextColumn = 0
+          // this pair is passed in as nextValidCoords
+      // if piecesCount < n
+        // set solution to recurse
+        // if (Array.isArray(solution){
+          // return solution
+      // else
+        // return board
+  // end
+  // row++
+  // columnIndex = 0;
+  */
   return solution;
 };
 
@@ -44,4 +108,25 @@ window.countNQueensSolutions = function(n) {
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
+};
+
+// Helper Functions:
+window.createSolution = function(n, board) {
+  var solution = [];
+  for (var i = 0; i < n; i++) {
+    solution.push(board.attributes[i].slice());
+  }
+  return solution;
+};
+
+window.cloneBoard = function(n, oldBoard) {
+  var newBoard = new Board({n: n});
+  for (var i = 0; i < n; i++) {
+    newBoard.attributes[i] = oldBoard.attributes[i].slice();
+  }
+  return newBoard;
+  // create new board
+  // iterate through old board
+  // set matching new board index to slice of oldBoard index
+  // return newBoard
 };
